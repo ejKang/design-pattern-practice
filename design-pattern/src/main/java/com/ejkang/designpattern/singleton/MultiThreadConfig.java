6ab14cead353ac2fb381d981a561ada9c6a8cc4b
ejkang <joomn11@gmail.com>
2021-05-12 18:03:39 +0900

    update singleton - multi thread example code

--- design-pattern/src/main/java/com/ejkang/designpattern/singleton/MultiThreadConfig.java
+++ design-pattern/src/main/java/com/ejkang/designpattern/singleton/MultiThreadConfig.java
@@ -3,12 +3,11 @@ package com.ejkang.designpattern.singleton;
 public class MultiThreadConfig {
 	private MultiThreadConfig() {}
 	
-//	private static class InnerClz() {
-//		private static final MultiThreadConfig config = new MultiThreadConfig();
-//	}
+	private static class InnerClz {
+		private static final MultiThreadConfig config = new MultiThreadConfig();
+	}
 	
 	public static MultiThreadConfig getInstance() {
-//		return InnerClz.config;
-		return null;
+		return InnerClz.config;
 	}
 }
