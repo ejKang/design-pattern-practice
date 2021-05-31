6e71abaac3b2931b74086d775874e5e9f9ead879
ejkang <joomn11@gmail.com>
2021-05-31 20:00:48 +0900

    add bridge pattern code
    bridge pattern example

---
+++ design-pattern/src/main/java/com/ejkang/designpattern/bridge/Hello.java
@@ -0,0 +1,9 @@
+package com.ejkang.designpattern.bridge;
+
+public class Hello {
+	
+	public String greeting() {
+		
+		return "Hello";
+	}
+}
---
+++ design-pattern/src/main/java/com/ejkang/designpattern/bridge/Main.java
@@ -0,0 +1,9 @@
+package com.ejkang.designpattern.bridge;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		System.out.println(hello.greeting());
+	}
+}
