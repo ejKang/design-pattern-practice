b6ed424cb52069aebdef909829736da26c438dac
ej <joomn11@gmail.com>
2021-06-10 23:32:07 +0900

    update decorator pattern main code

--- design-pattern/src/main/java/com/ejkang/designpattern/decorator/Main.java
+++ design-pattern/src/main/java/com/ejkang/designpattern/decorator/Main.java
@@ -11,5 +11,13 @@ public class Main {
 
         System.out.println("product : " + ssd256.product());
         System.out.println("price : " + ssd256.price());
+
+        Component p2 = new Product2();
+        Component i7_2 = new CpuI7(p2);
+        Component ssd256_2 = new Ssd256(i7_2);
+
+        System.out.println("product2 : " + ssd256_2.product());
+        System.out.println("price2 : " + ssd256_2.price());
+
     }
 }
